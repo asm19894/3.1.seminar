92335e7416cf5a92a55f36afbed62ad1d033af2e
Anna Smirnova <sanna_i@mail.ru>
2023-03-09 10:33:18 +0300

    Решение 002_Task

---
+++ 002_Task/Program.cs
@@ -0,0 +1,18 @@
+﻿// a = 25, b = 5 -> да
+// a  = 2, b = 10 -> нет
+// a = 9, b = -3 -> да
+// a = -3, b = 9-> нет
+
+Console.WriteLine("Введите число 'a'");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 'b'");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if(numberA == numberB * numberB)
+{
+    Console.WriteLine("This is true");  
+}
+else
+{
+    Console.WriteLine("This is folce");
+}
